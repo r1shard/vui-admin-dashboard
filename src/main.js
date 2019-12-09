2e355aab409f02a4ea2f5da386ed046ce56eb1d1
Richard Villalobos <richvillalobos87@gmail.com>
2019-12-09 15:46:55 -0800

    create redirects file for netlify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+require('@/global-styles/animate.css')
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="container"
+    :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
+  >
+    <RequestAccount />
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" v-show="isDarkMode" />
+      <img src="@/assets/DCHQ-dark.svg" v-show="!isDarkMode" />
+      <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Sign into Design+Code HQ</h4>
+      <input
+        type="email"
+        placeholder="Email"
+        :class="{'light-field':isDarkMode, 'dark-field':!isDarkMode}"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        :class="{'light-field':isDarkMode, 'dark-field':!isDarkMode}"
+      />
+      <button>Sign In</button>
+      <router-link
+        to="/recover"
+        :class="{'light-link':isDarkmode,'dark-link':!isDarkMode}"
+      >Forgot your password?</router-link>
+      <ThemeSwitch />
+    </div>
+  </div>
+</template>
+
+<script>
+import RequestAccount from "@/components/RequestAccount.vue";
+import ThemeSwitch from "@/components/ThemeSwitch.vue";
+
+export default {
+  name: "SignIn",
+  components: {
+    RequestAccount,
+    ThemeSwitch
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.login {
+  width: 400px;
+}
+</style>
